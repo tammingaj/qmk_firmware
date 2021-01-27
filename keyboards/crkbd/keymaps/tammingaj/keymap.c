f00bd42eea788376ea95474904960c1118a8ae70
janpeter <janpeter@bicat.com>
2021-01-27 21:10:39 +0100

    new keymap

--- keyboards/crkbd/keymaps/tammingaj/keymap.c
+++ keyboards/crkbd/keymaps/tammingaj/keymap.c
@@ -38,7 +38,7 @@ LSFT_T(KC_TAB), KC_EXLM,  KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                        KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS, KC_PLUS, KC_PMNS,  KC_EQL, KC_LCBR, KC_RCBR,                      KC_LBRC, KC_RBRC, KC_BSLS, KC_UNDS,   KC_NO, C(A(KC_DEL)),
+      KC_CAPS, KC_PLUS, KC_PMNS,  KC_EQL, KC_LCBR, KC_RCBR,                      KC_LBRC, KC_RBRC, KC_BSLS, KC_UNDS, KC_PIPE, C(A(KC_DEL)),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             KC_NO,   KC_NO, KC_TRNS,      KC_NO,   KC_NO,  KC_NO
                                       //`--------------------------'  `--------------------------'
@@ -46,13 +46,13 @@ LSFT_T(KC_TAB), KC_EXLM,  KC_AT, KC_HASH,  KC_DLR, KC_PERC,                     
 
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_NO, KC_HOME,   KC_UP,  KC_END, LCTL(KC_V),                   KC_PGUP,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_PSCR,
+       KC_TAB,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END, KC_LCTL, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, LCTL(KC_C),                   KC_PGDN,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, LCTL(KC_C), LCTL(KC_V),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, KC_LSFT,  KC_SPC,     KC_TRNS,  KC_NO,   KC_NO
+                                           KC_INS, KC_LSFT, KC_LCTL,    KC_TRNS,   KC_NO,   KC_NO
                                       //`--------------------------'  `--------------------------'
   )
 };
